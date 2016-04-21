518be5b3873610c068d46a8a416b3dc864bcf280
blachatdigital <josh@blackhatdigital.com.au>
2016-04-22 09:11:55 +1000

    Add name to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421015836) do
+ActiveRecord::Schema.define(version: 20160421230412) do
 
   create_table "records", force: :cascade do |t|
     t.text     "comments"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20160421015836) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
